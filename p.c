9f0f98f7139cb4c64b0ef38c52199bf5e521e61c
Tiago Barreiros de Freitas <tfbarreiros@fazenda.sp.gov.br>
2026-05-28 02:54:14 -0300

    Testando a câmera

--- p.c
+++ p.c
@@ -4,17 +4,17 @@
 #include <math.h>
 
 #define BACKGROUND (Color){ 0x13, 0x13, 0x13, 0xFF }
-#define CAMERA_SPEED 0.1f
+#define CAMERA_SPEED 1.1f
 #define ZOOM_SPEED 0.1f
 #define DEFAULT_ZOOM 3.0f
 #define MIN_X -10.0f*(DEFAULT_ZOOM)
 #define MAX_X -(MIN_X)
 #define MIN_Y -10.0f*(DEFAULT_ZOOM)
 #define MAX_Y -(MIN_Y)
-#define DEFAULT_STEP ((MAX_X)-(MIN_X))
+#define DEFAULT_STEP 30.0f
 #define TICK_STEP 1.0f
 #define TICK_SIZE 5.0f
-#define RESOLUTION 100.0f
+#define RESOLUTION 500.0f
 
 typedef struct {
     float zoom;
@@ -23,7 +23,7 @@ typedef struct {
 
 #define cartesian_to_screen(x, y, cartesian, offset_x, offset_y) (Vector2){ \
  ((x)-(offset_x))*(cartesian.x_scale) + (cartesian.center_screen_x), \
- ((y)-(offset_y))*(cartesian.y_scale) + (cartesian.center_screen_y)  \
+ -(((y)-(offset_y))*(cartesian.y_scale)) + (cartesian.center_screen_y)  \
 }
 
 typedef struct {
@@ -37,7 +37,7 @@ int main(void)
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 800, "p");
-    SetTargetFPS(60);
+    SetTargetFPS(30);
 
     PCamera camera = {0};
     camera.zoom = DEFAULT_ZOOM;
@@ -48,13 +48,13 @@ int main(void)
         const Cartesian cartesian = {
             .center_screen_x = screen_width/2,
             .center_screen_y = screen_height/2,
-            .x_scale = camera.zoom*screen_width/DEFAULT_STEP,
-            .y_scale = camera.zoom*screen_height/DEFAULT_STEP
+            .x_scale = camera.zoom*DEFAULT_STEP,
+            .y_scale = camera.zoom*DEFAULT_STEP
         };
         if (IsKeyPressed(KEY_Q)) {
             break;
         }
-        if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyDown(KEY_EQUAL)) {
+        if (IsKeyDown(KEY_EQUAL)) {
             camera.zoom += ZOOM_SPEED;
         }
         if (IsKeyDown(KEY_MINUS)) {
@@ -83,10 +83,12 @@ int main(void)
 
             ClearBackground(BACKGROUND);
 
-            float min_x = MIN_X/camera.zoom + camera.offset.x;
-            float max_x = MAX_X/camera.zoom + camera.offset.x;
-            float min_y = MIN_Y/camera.zoom + camera.offset.y;
-            float max_y = MAX_Y/camera.zoom + camera.offset.y;
+            
+            float max_x = (screen_width/DEFAULT_STEP)+camera.offset.x;
+            float min_x = -(screen_width/DEFAULT_STEP)+camera.offset.x;
+
+            float max_y = (screen_height/DEFAULT_STEP)+camera.offset.y;
+            float min_y = -(screen_height/DEFAULT_STEP)+camera.offset.y;
 
             {
                 Vector2 start_pos = cartesian_to_screen(min_x, 0, cartesian, camera.offset.x, camera.offset.y);
@@ -107,12 +109,21 @@ int main(void)
                 }
             }
             {
-                const float inc = (float)((max_x-min_x)/RESOLUTION);
-            for (float x = min_x; x <= max_x; x += inc) {
-                float y = -sinf(x);
-                Vector2 pos = cartesian_to_screen(x, y, cartesian, camera.offset.x, camera.offset.y);
-                DrawRectangleV(pos, (Vector2) { 2.0f, 2.0f }, RAYWHITE);
-            }
+                const float inc = (float)((max_x-min_x)/(RESOLUTION*sqrtf(camera.zoom)));
+
+                for (float x = min_x; x <= max_x; x += inc) {
+                    float y = sinf(x);
+                    Vector2 pos = cartesian_to_screen(x, y, cartesian, camera.offset.x, camera.offset.y);
+                    DrawRectangleV(pos, (Vector2) { 4.0f, 4.0f }, RED);
+
+                    y = x*x*x;
+                    pos = cartesian_to_screen(x, y, cartesian, camera.offset.x, camera.offset.y);
+                    DrawRectangleV(pos, (Vector2) { 4.0f, 4.0f }, SKYBLUE);
+
+                    y = -x;
+                    pos = cartesian_to_screen(x, y, cartesian, camera.offset.x, camera.offset.y);
+                    DrawRectangleV(pos, (Vector2) { 4.0f, 4.0f }, LIME);
+                }
             }
 
             DrawFPS(5, 5);
@@ -125,3 +136,6 @@ int main(void)
 
     return 0;
 }
+
+// TODO
+// 1. ajuster a velocidade de zoom e de movimento de acordo com o nível de zoom. Quando maior o zoom maior a velocidade movimento e maior a aproximação para mais-zoom.
